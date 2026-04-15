29836ea61b25f1f0a9a94174d08855aaab4cb47b
Yuii-Antoniuk <u983405@gmail.com>
2026-04-15 20:48:44 +0300

    fix x-scroll

--- js/project.js
+++ js/project.js
@@ -12,6 +12,6 @@
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
-    document.body.classList.toggle("no-scroll");
+//    document.body.classList.toggle("no-scroll");
   }
 })();
