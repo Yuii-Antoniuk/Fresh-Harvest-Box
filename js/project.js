cf103efa2a8e52f0ecab8a6422f6bccd9ec7ca3d
Nik-kul <nikitoskul17@gmail.com>
2026-04-16 18:20:23 +0300

    фінал-модалки

--- js/project.js
+++ js/project.js
@@ -12,6 +12,38 @@
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
-//    document.body.classList.toggle("no-scroll");
+//   document.body.classList.toggle("no-scroll");
   }
 })();
+
+
+const form = document.querySelector(".form");
+
+const orderModal = document.querySelector("[data-modal]");
+const successModal = document.querySelector("[data-modal-success]");
+const closeSuccess = document.querySelector("[data-close-success]");
+
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+
+  orderModal.classList.add("is-hidden");
+
+
+  successModal.classList.remove("is-hidden");
+});
+
+
+closeSuccess.addEventListener("click", () => {
+  successModal.classList.add("is-hidden");
+  document.body.style.overflow = "";
+});
+
+
+successModal.addEventListener("click", (e) => {
+  if (e.target === successModal) {
+    successModal.classList.add("is-hidden");
+    document.body.style.overflow = "";
+  }
+});
